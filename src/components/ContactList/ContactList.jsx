15266ab0905b3a9c824b88a3224685409ef69b75
NataliiaBoh <homesweethome160126@gmail.com>
2024-06-20 17:00:12 +0300

    apdate props

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,13 @@
 import css from "./ContactList.module.css";
 import Contact from "../Contact/Contact";
 
-export default function ContactList({ contacts, onContactDelate }) {
+export default function ContactList({ contacts, onContactDelete }) {
   return (
     <>
       <ul className={css.list}>
         {contacts.map((contact) => (
           <li className={css.listElem} key={contact.id}>
-            <Contact user={contact} onContactDelate={onContactDelate} />
+            <Contact user={contact} onContactDelate={onContactDelete} />
           </li>
         ))}
       </ul>
